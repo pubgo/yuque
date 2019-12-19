35f71b376c3ceb225e978467bac9773633d48cc9
kooksee <kooksee@163.com>
2019-12-19 14:55:31 +0800

    Version(v0.0.4) Date(2019-12-19 14:55:31))

--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
-const Version = "v0.0.3"
-const BuildV = "2019-12-13 14:41:06"
-const CommitV = "82cdc2db"
+const Version = "v0.0.4"
+const BuildV = "2019-12-19 14:55:31"
+const CommitV = "58be8ceb"
